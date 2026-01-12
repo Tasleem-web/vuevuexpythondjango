984985c734321e678c636a31916e07579417726f
Tasleem <tasleembca31@gmail.com>
2026-01-12 16:34:36 +0530

    change build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+
+pip install -r myenv/requirements.txt
+
 cd myenv/ecomproject 
-pip install -r ../requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
